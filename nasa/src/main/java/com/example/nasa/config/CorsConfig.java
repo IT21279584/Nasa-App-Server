620c70f97db4f215047f6253343b494708a60e1e
Hansaka Sudusinghe <91568447+IT21279584@users.noreply.github.com>
2024-05-04 23:31:35 +0530

    Update CorsConfig.java

--- nasa/src/main/java/com/example/nasa/config/CorsConfig.java
+++ nasa/src/main/java/com/example/nasa/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "https://spaceexplorer.vercel.app/") // Add your frontend URL here
+                .allowedOrigins("https://spaceexplorer.vercel.app/") // Add your frontend URL here
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
