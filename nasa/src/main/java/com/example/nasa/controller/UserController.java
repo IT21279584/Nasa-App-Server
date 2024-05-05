40ac6af8641921452d201f974250afdcfd0c923d
Hansaka Sudusinghe <91568447+IT21279584@users.noreply.github.com>
2024-05-05 10:10:08 +0530

    Update UserController.java

--- nasa/src/main/java/com/example/nasa/controller/UserController.java
+++ nasa/src/main/java/com/example/nasa/controller/UserController.java
@@ -9,11 +9,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.CrossOrigin;
+
 
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "https://spaceexplorer.vercel.app")
+
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
